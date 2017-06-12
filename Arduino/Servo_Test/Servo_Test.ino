36a74e53e0ac94d1f12e8801dbc523cb2820a89d
Ryan Calme <ryan.calme@gmail.com>
2017-06-12 14:05:51 -0600

    Test of servos, with serial comm to set angles

---
+++ Arduino/Servo_Test/Servo_Test.ino
@@ -0,0 +1,43 @@
+#include <Servo.h>
+
+Servo rightDrawerServo;
+Servo keyDrawerServo;
+const byte rightDrawerServoPin = 10;
+const byte keyDrawerServoPin = 11;
+int incomingByte = 0;
+
+void setup() {
+  // Enable serial connection
+  Serial.begin(9600);
+  
+  // Servos
+  rightDrawerServo.attach(rightDrawerServoPin);
+  keyDrawerServo.attach(keyDrawerServoPin);
+  
+  // On power up, we want this drawer to open
+  openRightDrawer();
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    incomingByte = Serial.parseInt();
+    Serial.print("Moving to ");
+    Serial.print(incomingByte, DEC);
+    Serial.println(" degrees...");
+    keyDrawerServo.write(incomingByte);
+  }
+}
+
+// Causes the servo to move such that the right drawer opens
+void openRightDrawer(){
+  Serial.println("Opening right drawer");
+  rightDrawerServo.write(90);  // 90 degrees
+  delay(500);
+}
+
+// Causes the servo to move such that the key drawer opens
+void openKeyDrawer(){
+  Serial.println("Opening key drawer");
+  keyDrawerServo.write(180);  // 180 degrees
+  delay(500);
+}
