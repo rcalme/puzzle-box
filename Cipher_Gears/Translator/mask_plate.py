6a441c0a9c829cd056fe8067c387f9ee4269dea7
Ryan Calme <ryan.calme@gmail.com>
2018-02-22 20:07:50 -0700

    More error checking, changed message

--- Cipher_Gears/Translator/mask_plate.py
+++ Cipher_Gears/Translator/mask_plate.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 import random
 import sys
+import string
 from termcolor import colored
 from cryptex import GearTranslator
 
+INTERSECT_CHARACTER = u"\u25A0"
 
 def create_random_grid(width, height):
-    letters = [x for x in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ']
+    letters = [x for x in string.ascii_uppercase] #'ABCDEFGHIJKLMNOPQRSTUVWXYZ']
     random.seed(1234)
     grid = []
     for xpos in range(height):
@@ -24,7 +26,7 @@ def print_grid(grid, colors=True):
             if colors:
                 sys.stdout.write(colored(char[0], char[1]))
             else:
-                sys.stdout.write(char)
+                sys.stdout.write(colored(char[0], 'white'))
             if idx != len(row)-1:
                 sys.stdout.write(' ')
         sys.stdout.write("|\n")
@@ -46,19 +48,20 @@ def create_mask_plate(width, height):
     for xpos in range(height):
         mask.append([])
         for ypos in range(width):
-            mask[xpos].append(' ')
+            mask[xpos].append((' ', 'white'))
 
     # Determine where on the full-sized grid, the side A holes will land
-    mask = apply_holes(mask, holes, 3, 1, 'A')
+    mask = apply_holes(mask, holes, 3, 1, 'A', 'blue')
     # Flip the holes plate top-to-bottom
     holes.reverse()
     # Determine where on the full-sized grid, the side A holes will land
-    mask = apply_holes(mask, holes, 10, 3, 'B')
+    mask = apply_holes(mask, holes, 10, 3, 'B', 'green')
    
     return mask
     
 
-def apply_holes(mask, holes, x_offset, y_offset, side):
+def apply_holes(mask, holes, x_offset, y_offset, side, color='white'):
+    intersect = False
     for row_idx, row in enumerate(holes):
         if row_idx + y_offset >= len(mask):
             continue
@@ -67,7 +70,16 @@ def apply_holes(mask, holes, x_offset, y_offset, side):
                 continue
             if holes[row_idx][col_idx] != ' ':
                 x_pos, y_pos = col_idx+x_offset, row_idx+y_offset
-                mask[y_pos][x_pos] = side
+                if mask[y_pos][x_pos][0] != ' ':
+                    mask[y_pos][x_pos] = (INTERSECT_CHARACTER, 'red')
+                    intersect = True
+                else:
+                    mask[y_pos][x_pos] = (side, color)
+
+    if intersect:
+        print "Mask holes would intersect in this configuration!"
+        print ""
+                
     return mask
     
 
@@ -77,8 +89,12 @@ def mask_message(grid, mask, side, message):
     for row_idx, row in enumerate(grid):
         masked.append([])
         for col_idx, col in enumerate(grid[row_idx]):
-            if mask[row_idx][col_idx] == side and msg_idx < len(message):
-                masked[row_idx].append((message[msg_idx], 'blue'))
+            mask_char = mask[row_idx][col_idx]
+            if mask_char[0] == side and msg_idx < len(message):
+                masked[row_idx].append((message[msg_idx], mask_char[1]))
+                msg_idx += 1
+            elif mask_char[0] == INTERSECT_CHARACTER and msg_idx < len(message):
+                masked[row_idx].append(mask_char)
                 msg_idx += 1
             else:
                 masked[row_idx].append(grid[row_idx][col_idx])
@@ -95,17 +111,16 @@ def main():
 
     mask = create_mask_plate(width, height)
     print "Two-sided mask with holes"
-    print_grid(mask, colors=False)
+    print_grid(mask)
  
     message_a = 'MAGNETCOMBINATIONCODEISHRJAS'
-    message_b = 'WIREUPGRANDCHILDRENBIRTHDAYS'
-    message_c = 'KNOCKSHAVEANDAHAIRCUTTWOBITS'
+    message_b = 'WIREUPYOURCHILDRENSBIRTHDAYS'
 
     # Prepare to encrypt
     gt = GearTranslator(2684)
 
     # Prepare side A
-    gt.select_and_set_gears([2,4,5], "DAD")
+    gt.select_and_set_gears([2,4,5], "BOX")
     masked_front = mask_message(
         grid,
         mask,
@@ -116,7 +131,7 @@ def main():
     print_grid(masked_front)
 
     # Prepare side B
-    gt.select_and_set_gears([4,3,6], "BEE")
+    gt.select_and_set_gears([4,3,6], "PUZ")
     masked_back = mask_message(
         masked_front,
         mask,
